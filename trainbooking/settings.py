f5375fe2662e2f78de3df9f579ed667d21b9af5a
kavyakumari1725-rgb <kavyakumari1725@gmail.com>
2025-12-15 22:05:24 +0000

    Fix static files and whitenoise

--- trainbooking/settings.py
+++ trainbooking/settings.py
@@ -65,6 +65,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
